1c455a19687e2fd827ec7810315a651c58e731c2
Tanuka Das <das.tanuka@yahoo.com>
2019-04-19 22:24:06 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,22 +4,32 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  differs = num1 -num2 
+  differs = num1 -num2
   return differs
 end
 
 def division(num1, num2)
+  result = num1/num2
+  return result
 end
 
 def multiplication(num1, num2)
+  result = num1 * num2
+  return result
 end
 
 def modulo(num1, num2)
+  result = num1 % num2
+  return result
 end
 
 def square_root(num)
+  result = num ** 2
+  return result
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  result = num1 + ((num2 * num3) / num4)
+  return result
 end
